97df31c9d174666c37517868bef80d6a5b691b7c
Matheus Henrique <matheus.181137@gmail.com>
2023-11-02 17:47:18 -0300

    feat: setup eslint with airbnb style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.js'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'react-rules'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
