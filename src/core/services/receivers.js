49c7d3eb9b9c3fe6f9b0b9b2a80264e2ce4d63a1
Matheus Henrique <matheus.181137@gmail.com>
2023-11-03 11:33:50 -0300

    feat: create receiver creation service

--- src/core/services/receivers.js
+++ src/core/services/receivers.js
@@ -9,6 +9,15 @@ const receiversService = {
       `${import.meta.env.VITE_TRANSFEERA_API_URL}/receivers/${receiverId}`,
     ).then((res) => res.json());
   },
+  createReceiver(receiver) {
+    return fetch(`${import.meta.env.VITE_TRANSFEERA_API_URL}/receivers`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(receiver),
+    }).then((res) => res.json());
+  },
 };
 
 export default receiversService;
