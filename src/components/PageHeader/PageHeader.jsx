31f4a4bbbe522f28dda47e5d8307085a8517accb
Matheus Henrique <matheus.181137@gmail.com>
2023-11-05 11:29:31 -0300

    feat: add input component to layout

--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -1,10 +1,13 @@
-import { Button, CircleButton } from '../Button/Button';
+import { CircleButton } from '../Button/Button';
+import { InputWithSuffix } from '../Input/Input';
 import plusIcon from '../../assets/plus-icon.svg';
+import searchLogo from '../../assets/search-icon.svg';
 
 import {
   PageHeaderCTAButton,
   PageHeaderContainer,
   PageHeaderPlusIcon,
+  PageHeaderSearchIcon,
   PageHeaderTitle,
   PageHeader as SPageHeader,
 } from './PageHeader.styles';
@@ -14,11 +17,13 @@ export const PageHeader = () => {
     <SPageHeader>
       <PageHeaderContainer>
         <PageHeaderTitle>Seus favorecidos</PageHeaderTitle>
-        <Button>Salvar</Button>
         <CircleButton $size={41} $customCss={PageHeaderCTAButton}>
           <PageHeaderPlusIcon src={plusIcon} />
         </CircleButton>
       </PageHeaderContainer>
+      <InputWithSuffix>
+        <PageHeaderSearchIcon src={searchLogo} />
+      </InputWithSuffix>
     </SPageHeader>
   );
 };
--- src/components/PageHeader/PageHeader.styles.js
+++ src/components/PageHeader/PageHeader.styles.js
@@ -4,12 +4,16 @@ export const PageHeader = styled.section`
   height: 122px;
   width: 100%;
   background-color: ${(props) => props.theme.colors.gray700};
+  display: flex;
+  align-items: center;
+  padding: 40px 44px;
 `;
 
 export const PageHeaderContainer = styled.div`
   height: 100%;
   width: 100%;
-  padding: 40px 44px;
+  display: flex;
+  align-items: center;
 `;
 
 export const PageHeaderTitle = styled.h1`
@@ -27,4 +31,10 @@ export const PageHeaderCTAButton = css`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-left: 8px;
+`;
+
+export const PageHeaderSearchIcon = styled.img`
+  height: 16px;
+  width: 16px;
 `;
