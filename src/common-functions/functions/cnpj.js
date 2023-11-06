35b43ad64a21aa868926d190a7315321c8fd8985
Matheus Henrique <matheus.181137@gmail.com>
2023-11-06 20:22:21 -0300

    feat: create validation functions

---
+++ src/common-functions/functions/cnpj.js
@@ -0,0 +1,31 @@
+const getRemaining = (value) => (value % 11 < 2 ? 0 : 11 - (value % 11));
+
+const generateCheckSums = (numbers, validators) => {
+  const initialCheckSums = [0, 0];
+
+  return validators.reduce(
+    ([checkerA, checkerB], validator, index) => [
+      index === 0 ? 0 : checkerA + numbers[index - 1] * validator,
+      checkerB + numbers[index] * validator,
+    ],
+    initialCheckSums,
+  );
+};
+
+const isRepeatedArray = (items) => items.every((item) => items[0] === item);
+
+const mapToNumeric = (value) => value.replace(/\D/g, '');
+
+const mapToNumbers = (value) => mapToNumeric(value).split('').map(Number);
+
+export const validateCNPJ = (value) => {
+  if (!/^(\d{14}|\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2})$/.test(value)) return false;
+  const numbers = mapToNumbers(value);
+  if (isRepeatedArray(numbers)) return false;
+  const validators = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  const checkers = generateCheckSums(numbers, validators);
+  return (
+    numbers[12] === getRemaining(checkers[0]) &&
+    numbers[13] === getRemaining(checkers[1])
+  );
+};
---
+++ src/common-functions/index.js
@@ -0,0 +1,5 @@
+export * from './functions/cpf';
+export * from './functions/cnpj';
+export * from './functions/phone';
+export * from './functions/string';
+export * from './functions/uuid';
