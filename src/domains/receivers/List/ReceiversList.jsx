bc93353b94ffb04476c464cea40cd026c06a21dd
Matheus Henrique <matheus.181137@gmail.com>
2023-11-05 21:53:33 -0300

    feat: passing open modal prop

--- src/domains/receivers/List/ReceiversList.jsx
+++ src/domains/receivers/List/ReceiversList.jsx
@@ -2,10 +2,10 @@ import { PageHeader } from '../../../components';
 import { ReceiverListContainer } from '../styles/receivers.styles';
 import { ReceiverTable } from './ReceiverTable';
 
-export const ReceiverList = () => {
+export const ReceiverList = ({ onOpenModal }) => {
   return (
     <>
-      <PageHeader />
+      <PageHeader onOpenModal={onOpenModal} />
       <ReceiverListContainer>
         <ReceiverTable />
       </ReceiverListContainer>
