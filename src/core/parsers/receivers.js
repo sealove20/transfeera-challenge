f472cd233653b1924fbad41f524b400721d7bd87
Matheus Henrique <matheus.181137@gmail.com>
2023-11-07 09:23:27 -0300

    feat: return sorted receivers list

--- src/core/parsers/receivers.js
+++ src/core/parsers/receivers.js
@@ -1,3 +1,9 @@
+const sortByName = (a, b) => {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+};
+
 export const ReceiverParser = {
   single: (receiver) => {
     const {
@@ -36,7 +42,7 @@ export const ReceiverParser = {
   },
   list: (receivers = []) => {
     return {
-      receivers: receivers.map(ReceiverParser.single),
+      receivers: receivers.map(ReceiverParser.single).sort(sortByName),
     };
   },
 };
