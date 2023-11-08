a7dbc7fb8759940474ca6883f5038063875bc4a2
Matheus Henrique <matheus.181137@gmail.com>
2023-11-08 10:48:24 -0300

    feat: add loading to table component

--- src/components/Table/TableBody/TableBody.jsx
+++ src/components/Table/TableBody/TableBody.jsx
@@ -1,3 +1,16 @@
-export const TableBody = ({ children }) => {
+import { Loading } from '../../Loading/Loading';
+import { TableBodyCell } from '../TableBodyCell/TableBodyCell';
+import { tableBodyCenteredCell } from './TableBody.styles';
+
+export const TableBody = ({ children, isLoading = false }) => {
+  if (isLoading) {
+    return (
+      <TableBodyCell colSpan="100%">
+        <div css={tableBodyCenteredCell}>
+          <Loading />
+        </div>
+      </TableBodyCell>
+    );
+  }
   return <tbody>{children}</tbody>;
 };
