b6004867510d304440bdc7f1833ae2475b0bd376
Matheus Henrique <matheus.181137@gmail.com>
2023-11-08 10:47:12 -0300

    feat: crete loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,9 @@
+import { Loading as SLoading, LoadingSpinner } from './Loading.styles';
+
+export const Loading = () => {
+  return (
+    <SLoading>
+      <LoadingSpinner />
+    </SLoading>
+  );
+};
